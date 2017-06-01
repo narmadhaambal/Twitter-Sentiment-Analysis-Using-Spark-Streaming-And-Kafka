9783b535c486fe8cac08bfa78d1200379a61c828
Narmadha Ambalavanan <Narmadha@Narmadhas-MacBook-Air.local>
2017-05-31 21:33:05 -0700

    adding tab

--- twitterStream.py
+++ twitterStream.py
@@ -20,6 +20,7 @@ def main():
 
 
 def make_plot(counts):
+
     """
     This function plots the counts of positive and negative words for each timestep.
     """
@@ -28,13 +29,14 @@ def make_plot(counts):
     time = []
 
     for val in counts:
-	   positiveTuple = val[0]
-	   positiveCounts.append(positiveTuple[1])
-	   negativeTuple = val[1]
-	   negativeCounts.append(negativeTuple[1])
+
+	    positiveTuple = val[0]
+	    positiveCounts.append(positiveTuple[1])
+	    negativeTuple = val[1]
+	    negativeCounts.append(negativeTuple[1])
 
     for i in range(len(counts)):
-	   time.append(i)
+	    time.append(i)
 
     posLine = plt.plot(time, positiveCounts,'bo-', label='Positive')
     negLine = plt.plot(time, negativeCounts,'go-', label='Negative')
